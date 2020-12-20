8e78c06a41ca5b98c95c997bb1f9fd2d40c36908
Bl4Cc4t <kuron3ko@cock.li>
2020-12-20 14:09:17 +0100

    first commit.

---
+++ google_search.pre21style.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name          Google Search Pre 2021 Style
+// @version       0.1
+// @description   Small script to get the non-experimental look without Google Sans back (https://imgur.com/a/pbjChlu)
+// @author        schwarzkatz
+// @match         https://www.google.com/search*
+// @grant         GM_addStyle
+// @updateURL     https://github.com/Bl4Cc4t/userscripts-public/raw/master/google_search.pre21style.user.js
+// @downloadURL   https://github.com/Bl4Cc4t/userscripts-public/raw/master/google_search.pre21style.user.js
+// ==/UserScript==
+
+(function() {
+  GM_addStyle(`
+    * {
+      font-family: arial, sans-serif !important;
+    }
+    /* Smaller headers */
+    #res h3, #botstuff h3 {
+      font-size: 20px !important;
+      line-height: 26px !important;
+    }
+
+    /* smaller, hover-only box shadow */
+    form[role=search] .logo.doodle + div {
+      box-shadow: none !important;
+      border-color: rgb(223, 225, 229) !important;
+    }
+    form[role=search] .logo.doodle + div:hover {
+      box-shadow: 0 1px 6px rgba(32,33,36,.28) !important;
+    }
+  `)
+
+  // removes the font styling completely, some elements (People also ask/h2) are not matched by the css above, since they have an !important flag
+  Array.from(document.querySelectorAll("style")).find(e => e.textContent.match(/^@font-face/)).remove()
+})()
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Public userscripts
+Just some smaller public userscripts.
+Larger projects:
+- [GoodTwitter2](https://github.com/Bl4Cc4t/GoodTwitter2)
+
+
+## Content
+- [Google Search Pre 2021 Style](#google-search-pre-2021-style)
+
+
+### Google Search Pre 2021 Style
+Small script to get the non-experimental look without Google Sans back
+
+<details>
+<summary>Preview</summary>
+with Google Sans | standard look
+:-:|:-:
+![](https://i.imgur.com/VasKr6x.png) | ![](https://i.imgur.com/gFdYTo7.png)
+</details>
