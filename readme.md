a90986edda6866dc94eff048291484a85357454c
Bl4Cc4t <kuron3ko@cock.li>
2022-02-18 20:35:52 +0100

    added Yandex - Always disable safe search

--- readme.md
+++ readme.md
@@ -12,6 +12,7 @@ Larger project(s): [GoodTwitter2](https://github.com/Bl4Cc4t/GoodTwitter2)
 - [MyHeritage Open links in new tab](#myheritage---open-links-in-new-tab)
 - [Picarto Pre 2021 Style](#picarto---pre-2021-style)
 - [Reddit Always Disable Safe Search](#reddit---always-disable-safe-search)
+- [Yandex Always Disable Safe Search](#yandex---always-disable-safe-search)
 
 
 ## Google Search - Pre 2021 style
@@ -33,3 +34,6 @@ Picarto with the old style. ([install](https://github.com/Bl4Cc4t/userscripts-pu
 
 ## Reddit - Always disable safe search
 Always disable the safe search toggle when performing a search on reddit. ([install](https://github.com/Bl4Cc4t/userscripts-public/raw/master/reddit.always-disable-safe-search.user.js))
+
+## Yandex - Always disable safe search
+Always disable the safe search toggle when performing a search on yandex. ([install](https://github.com/Bl4Cc4t/userscripts-public/raw/master/yandex.always-disable-safe-search.user.js))
---
+++ yandex.always-disable-safe-search.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name          Yandex - Always disable safe search
+// @version       0.1
+// @description   Always disable the safe search toggle when performing a search on yandex.
+// @author        schwarzkatz
+// @match         https://yandex.com/*
+// @grant         none
+// @updateURL     https://github.com/Bl4Cc4t/userscripts-public/raw/master/yandex.always-disable-safe-search.user.js
+// @downloadURL   https://github.com/Bl4Cc4t/userscripts-public/raw/master/yandex.always-disable-safe-search.user.js
+// ==/UserScript==
+
+(function() {
+  // wait for element to exist
+  // https://stackoverflow.com/a/61511955
+  function waitForElm(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector))
+      }
+
+      const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector))
+          observer.disconnect()
+        }
+      })
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      })
+    })
+  }
+
+  waitForElm(`.head-filter_type_moderate`).then(() => $(`[role=menuitem] > [data-bem*=unlimited]`).click())
+})()
